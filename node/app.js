63c74cbb931efc69b5f0ad98994eb2226d1dce9b
Wladimir <wladimirss14@gmail.com>
2022-11-13 15:09:30 -0400

    desde aqui iniciamos con .json

--- node/app.js
+++ node/app.js
@@ -1,8 +1,37 @@
-// const saludos = require('./saludos.js');
-const { saludarHolaMundo } = require('./saludos.js');
+const fs = require('fs');
+// const { TextEncoderStream } = require('node:stream/web');
 
-console.log(saludarHolaMundo());
+// fs.readFile('index.html', 'utf-8', (err, contenido) => {
+//   if (err){
+//     // console.log(err);
+//     throw err;
+//   }else{
+//     console.log(contenido);
+//   }
+//   console.log('Mensaje...')
+// });
 
-// console.log(saludo.saludar('Wladimir'));
-// console.log(saludos.saludar('Wladimir'));
-// console.log(saludos.saludarHolaMundo());
+// fs.rename('./juan.html', 'index.html', (err)=>{
+//   if (err){
+//     console.log('no se realizo el cambio')
+//     throw TextEncoderStream;
+//   }
+//   console.log('cambio exitoso');
+// })
+
+// fs.unlink('./main.html', (err) =>{
+//   if (err){
+//     throw err;
+//   }
+//   console.log('Eliminado con exito');
+// })
+
+// for ( i = 1; i<100000; i++) {
+
+//   fs.appendFileSync('./virus.txt', i + ' soy un gusano \n', (err)=>{
+//     if (err){
+//       throw err
+//     }
+//     console.log(`virus ejecutandose ${i}`);
+//   });
+// };
