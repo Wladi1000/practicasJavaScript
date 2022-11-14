9e383f37a530a850a71d290b4c0ce57f087c469d
Wladimir <wladimirss14@gmail.com>
2022-11-14 14:26:59 -0400

    Se añadieron parametros

--- node/app.js
+++ node/app.js
@@ -3,8 +3,9 @@ const EventEmitter = require('events');
 
 const emisorProductos = new EventEmitter();
 
-emisorProductos.on('compra', () => {
-  console.log('Se realizo una compra.');
+
+emisorProductos.on('compra', (total) => {
+  console.log(`Se realizo una compra por $${total}`);
 });
 
-emisorProductos.emit('compra');
+emisorProductos.emit('compra', 500);
