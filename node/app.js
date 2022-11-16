69ee3e6a6f9a41750d18c1ac92a1416580132f4c
Wladimir <wladimirss14@gmail.com>
2022-11-15 20:45:30 -0400

    promesas concatenadas para su sincronismo

--- node/app.js
+++ node/app.js
@@ -1,31 +1,44 @@
-const statusPedido = () =>{
-  return Math.random() < 0.8;
+// La promesa retorna una cadena
+const ordenarProducto = (producto) =>{
+
+  return new Promise ( (resolve, reject) =>{
+
+    console.log(`Ordenado: ${producto} de freeCodeCamp.`)
+    setTimeout(() => {
+      producto === 'taza'? resolve('Ordenando una taza con el logo de freeCodeCamp...'): reject('Este producto no esta disponible actualmente.');
+    
+    }, 2000);
+  } );
+}
+// Usa la abreviacion clg para invocar un console.log
+const procesarPedido = (respuesta)=>{
+  // 
+  // Si una funcion flecha recibe un parametro no es necvesario
+  // rodearlo entre parentesis, pero lo hago por buena practica
+  // 
+  return new Promise((resolve) => {
+
+    console.log('Procesando respuesta...');
+    console.log(`La respuesta fue "${respuesta}"`);
+    setTimeout(() => {
+      resolve('Gracias por su compra. Disfruta tu producto de freeCodeCamp');
+    }, 4000);
+  });
 };
 
-const miPedidoPizza = new Promise((resolve, reject) =>{
+// Anidar promesas asegura que se ejecuten una despues de otra, si se deja
+// cada una de forma independeinte no se asegura que una se haga despues
+// de la otra.
+ordenarProducto('lapiz')
+  .then(respuesta =>{
+    console.log('Respuesta recibida');
+    console.log(respuesta);
 
-  setTimeout(() => {
-    statusPedido() ? resolve('Pedido exitoso! Pizza esta en camino'): reject('transaccion fallida. Intente de nuevo');
-  }, 10);
-});
-
-
-// miPedidoPizza.then(manejarPedido, rechazarPedido);
-
-// miPedidoPizza
-//   .then((mensajeConfirmacion) => {
-//     console.log(mensajeConfirmacion);
-//   })
-//   .cath((mensajeError) =>{   //Cambio de then a cath
-//     console.log(mensajeError);
-//   })
-
-const manejarPedido = (mensajeConfirmacion) => {
-  console.log(mensajeConfirmacion);
-};
-
-const rechazarPedido = (mensajeError) =>{
-  console.log(mensajeError);
-};
-
-miPedidoPizza.then(manejarPedido).catch(rechazarPedido)
+    return procesarPedido(respuesta); // Esta llamada retorna una promesa
+  })
+  .then((respuestaProcesada)=>{
+    console.log(respuestaProcesada);
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
