05f545b08a269ca1e51014e10ce80b630b598c4b
Wladimir <wladimirss14@gmail.com>
2022-11-18 00:03:19 -0400

    Se ha hevho una introducion a Http y primer servidor creado

--- node/app.js
+++ node/app.js
@@ -1,62 +1,13 @@
-// La promesa retorna una cadena
-const ordenarProducto = (producto) =>{
+const http = require('http');
 
-  return new Promise ( (resolve, reject) =>{
+const servidor = http.createServer((req, res)=>{
+  // Proceso
+  console.log('Solicitud nueva');
+  res.end('Hola mundo');
+});
 
-    console.log(`Ordenado: ${producto} de freeCodeCamp.`)
-    setTimeout(() => {
-      producto === 'taza'? resolve('Ordenando una taza con el logo de freeCodeCamp...'): reject('Este producto no esta disponible actualmente.');
-    
-    }, 2000);
-  } );
-}
-// Usa la abreviacion clg para invocar un console.log
-const procesarPedido = (respuesta)=>{
-  // 
-  // Si una funcion flecha recibe un parametro no es necvesario
-  // rodearlo entre parentesis, pero lo hago por buena practica
-  // 
-  return new Promise((resolve) => {
+const PUERTO = 3000;
 
-    console.log('Procesando respuesta...');
-    console.log(`La respuesta fue "${respuesta}"`);
-    setTimeout(() => {
-      resolve('Gracias por su compra. Disfruta tu producto de freeCodeCamp');
-    }, 4000);
-  });
-};
-
-// Anidar promesas asegura que se ejecuten una despues de otra, si se deja
-// cada una de forma independeinte no se asegura que una se haga despues
-// de la otra.
-// ordenarProducto('lapiz')
-//   .then(respuesta =>{
-//     console.log('Respuesta recibida');
-//     console.log(respuesta);
-
-//     return procesarPedido(respuesta); // Esta llamada retorna una promesa
-//   })
-//   .then((respuestaProcesada)=>{
-//     console.log(respuestaProcesada);
-//   })
-//   .catch((err)=>{
-//     console.log(err);
-//   });
-
-// ------------------------------------------------------------------------
-
-// Con Asyncn y await nos permite escribir codigo asincrono de forma sincrona
-
-async function realizarPedido(producto){
-
-  try {
-    const respuesta = await ordenarProducto(producto);
-    console.log('Respuesta recibida');
-    const respuestaProcesada = await procesarPedido(respuesta);
-    console.log(respuestaProcesada);
-  } catch (error) {
-      console.log(error);
-  }
-}
-
-realizarPedido('taza');
+servidor.listen(PUERTO, () =>{
+  console.log(`El servidor esta escuchando http://localhost:${PUERTO}...`);
+});
