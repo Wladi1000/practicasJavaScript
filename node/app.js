a029a183da845a8a84b5410dc3e9eccf3eddd525
Wladimir <wladimirss14@gmail.com>
2022-11-19 21:01:47 -0400

    Se ha realizado un servidor con Node.js puro

--- node/app.js
+++ node/app.js
@@ -1,21 +1,66 @@
-const statusPedido = () =>{
-  return Math.random() < 0.8;
-};
+const http = require('http');
+const cursos = require('./cursos.js');
 
-const miPedidoPizza = new Promise((resolve, reject) =>{
+const servidor = http.createServer((req, res) =>{
 
-  setTimeout(() => {
-    statusPedido() ? resolve('Pedido exitoso! Pizza esta en camino'): reject('transaccion fallida. Intente de nuevo');
-  }, 3000);
+  const {method} = req;
+
+  switch (method) {
+    case 'GET':
+      return manejarSolicitudGet(req, res);
+      break;
+    case 'POST':
+      return manejarSolicitudPOST(req, res);
+      break;
+    default:
+      console.log(`El metodo usado no puede ser manejado por el servidor. Y el metodo es ${method}`);
+      break;
+  }
+
 });
 
+function manejarSolicitudGet(req, res){
+  let path = req.url;
 
-const manejarPedido = (mensajeConfirmacion) => {
-  console.log(mensajeConfirmacion);
+  console.log(res.statusCode);
+
+  if ( path === '/' ){
+
+    res.statusCode = 200;
+    return res.end('Bienvenidos a mi primer servidor y API creado por Node.js');
+  } else if( path === '/cursos' ){
+
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos));
+  }else if(path === '/cursos/programacion'){
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos.programacion));
+  }else if(path === '/cursos/matematicas'){
+    res.statusCode = 200;
+    return res.end(JSON.stringify(cursos.infoCursos.matematicas));
+  }else{
+    res.statusCode = 404;
+    return res.end('El recurso solicitado no existe...');
+  }
 };
 
-const rechazarPedido = (mensajeError) =>{
-  console.log(mensajeError);
-};
+function manejarSolicitudPOST(req, res) {
+  const path = req.url;
+  
+  if (path === '/cursos/programacion'){
 
-miPedidoPizza.then(manejarPedido, rechazarPedido);
+    res.statusCode = 200;
+    res.end('El servidor recibio una solicitud POST en programacion')
+  }
+}
+
+
+
+
+
+
+// iniciar servidor
+const PUERTO = 3000;
+servidor.listen(PUERTO, ()=>{
+  console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`);
+});
---
+++ node/cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+  'programacion':[
+    {
+      id: 1,
+      titulo: 'Aprende Python',
+      lenguaje: 'Python',
+      vistas: 15000,
+      nivel: 'basico'
+    },
+    {
+      id: 2,
+      titulo: 'Aprende Python Intermedio',
+      lenguaje: 'Python',
+      vistas: 13553,
+      nivel: 'intermedio'
+    },
+    {
+      id: 3,
+      titulo: 'Aprende JavaScript',
+      lenguaje: 'javascript',
+      vistas: 16420,
+      nivel: 'basico'
+    }
+  ],
+  'matematicas':[
+    {
+      id: 1,
+      titulo: 'Aprende Calculo',
+      tema: 'calculo',
+      vistas: 12455,
+      nivel: 'basico'
+    },
+    {
+      id: 2,
+      titulo: 'Aprende Algebra',
+      tema: 'algebra',
+      vistas: 54886,
+      nivel: 'intermedio'
+    }
+  ]
+}
+
+module.exports.infoCursos = infoCursos;
