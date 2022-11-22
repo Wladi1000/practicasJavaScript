7f7c813beb1ee4286dc0d60de344e4e8f1ed7888
Wladimir <wladimirss14@gmail.com>
2022-11-22 18:24:40 -0400

    Agregando CRUD y detalles con metodos para evitar extension de codigo... FIN DE CURSO

--- node/routers/programacion.js
+++ node/routers/programacion.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const { infoCursos } = require('../data/cursos');
 
 const {programacion} = require('../data/cursos').infoCursos;
 
 const routerProgramacion = express.Router();
 
+// Middleware
+routerProgramacion.use(express.json());
+// ---------------------------------------
+
 routerProgramacion.get('/', (req, res) =>{
-  res.send(JSON.stringify(programacion));
+  res.json(programacion);
 });
 
 routerProgramacion.get('/:lenguaje', (req, res) =>{
@@ -23,7 +28,7 @@ routerProgramacion.get('/:lenguaje', (req, res) =>{
     return res.send( JSON.stringify( resultados.sort( (a, b) => a.vistas - b.vistas ) ) );
   }
 
-  res.status(200).send(JSON.stringify(resultados));
+  res.status(200).json(resultados);
 
 });
 
@@ -35,11 +40,77 @@ routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
   const resultados = infoCursos.programacion.filter( (curso) => curso.lenguaje === lenguaje && curso.nivel === nivel);
 
   if ( resultados.length === 0 ){
-    return res.status(404).send(`No se encontraron cursos de ${lenguaje} de ${nivel}`);
+    // respuesta con cuerpo
+    return res.status(204).send(`No se encontraron cursos de ${lenguaje} de ${nivel}`);
+
+    // respuesta vacia
+    // return res.status(404).end();
+
   }
 
-  res.status(200).send(JSON.stringify(resultados));
+  res.status(200).json(resultados);
 
+});
+
+
+// Añadir
+routerProgramacion.post('/', (req, res) => {
+
+  let cursoNuevo = req.body;
+
+  programacion.push(cursoNuevo);
+  
+  rjson(programacion);
+
+});
+
+// Actualizar un objeto completo
+routerProgramacion.put('/:id', (req, res) =>{
+
+  const cursoActualizado = req.body;
+
+  const id = req.params.id;
+
+  const indice = programacion.findIndex( (curso) => curso.id == id);
+
+  if ( indice >= 0 ){
+    programacion[indice]= cursoActualizado;
+  }
+
+  res.json(programacion);
+});
+
+
+// Actualizar propiedades especificas de un objeto
+routerProgramacion.patch('/:id', (req, res) =>{
+
+  const infoActualizada = req.body;
+  
+  const id = req.params.id;
+
+  const indice = programacion.findIndex(curso => curso.id == id);
+
+  if ( indice >= 0 ){
+    const cursoModificar = programacion[indice];
+
+    Object.assign(cursoModificar, infoActualizada);
+  }
+  res.json(programacion);
+});
+
+
+// Elinimar un curso del arreglo de cursos
+routerProgramacion.delete('/:id', (req, res) =>{
+
+  const id = req.params.id;
+
+  const indice = programacion.findIndex(curso => curso.id == id)
+
+  if ( indice >=0 ){
+    programacion.splice(indice, 1);
+  }
+
+  res.json(programacion);
 });
 
 // Si queremos asignarlo directamente a la constante lo que hay
